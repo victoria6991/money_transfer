4f163c27f7e454de906d4f3c147d800708fc0c37
victoria6991 <miss.victoria-liar@yandex.ru>
2021-11-25 20:57:07 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 20_000_000_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
+        System.out.println(total);
+    }
+}
